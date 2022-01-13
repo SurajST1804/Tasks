4d796967d545e40dddc932274c633e9a059dc629
SURAJ S T <surajst.17is@saividya.ac.in>
2022-01-13 15:44:11 +0530

    Searching using ascending and descending order

---
+++ src/Home.jsx
@@ -0,0 +1,83 @@
+import React, { useEffect, useState } from "react";
+import Axios from "./Axios";
+
+const Home = () => {
+  let [state, setState] = useState([]);
+  let [searchTitle, setSearchTitle] = useState("");
+  let [ascending, setAscending] = useState([]);
+  let [descending, setDescending] = useState([]);
+
+  useEffect(() => {
+    let Data = async () => {
+      let finalData = await Axios.get("/posts");
+      setState(finalData.data);
+      //   console.log(finalData);
+    };
+    Data();
+  }, []);
+
+  let handleAscending = (e) => {
+    e.preventDefault();
+    let ascending = state.sort((a, b) => {
+      return a.id - b.id;
+    });
+    setAscending(ascending);
+  };
+
+  let handleDescending = (e) => {
+    e.preventDefault();
+    let descending = state.sort((a, b) => {
+      return b.id - a.id;
+    });
+    setDescending(descending);
+  };
+
+  // let hasc = ascending.map((a) => {
+  //   return <p>{a.title}</p>;
+  // });
+
+  // let hdes = descending.map((b) => {
+  //   return <p>{b.title}</p>;
+  // });
+
+  const displayTitle = state
+    .filter((val, ind) => {
+      if (searchTitle == "") {
+        return val;
+      } else if (val.title.toLowerCase().includes(searchTitle.toLowerCase())) {
+        return val;
+      }
+    })
+    .map((x) => {
+      return (
+        <div key={x.id}>
+          <h2>Id:{x.id}</h2>
+          <p>{x.title}</p>
+        </div>
+      );
+    });
+
+  return (
+    <section id="searchBlock">
+      <article>
+        <form>
+          <input
+            type="search"
+            placeholder="Search title ..."
+            onChange={(e) => {
+              setSearchTitle(e.target.value);
+            }}
+          />
+          <button onClick={handleAscending}>Ascending</button>
+          <button onClick={handleDescending}>Descending</button>
+        </form>
+        <h1>Titles:</h1>
+        <h1>{displayTitle}</h1>
+        {/* <h1>{hasc}</h1>
+        <h1>{hdes}</h1> */}
+      </article>
+    </section>
+  );
+};
+
+export default Home;
